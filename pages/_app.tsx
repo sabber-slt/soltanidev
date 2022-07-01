815f518bca9ea4640c4c42c67a7eb892acbb9d9a
Sabber-Soltani <96610564+sabber-slt@users.noreply.github.com>
2022-07-02 00:59:58 +0430

    add document

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,7 +6,6 @@ import '@fontsource/lalezar';
 import { Hydrate, QueryClient, QueryClientProvider } from 'react-query';
 import { ReactQueryDevtools } from 'react-query/devtools';
 import Layout from '../components/Layout';
-
 import Head from 'next/head';
 
 const AppContainer = styled.div`
@@ -26,22 +25,10 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-        <meta charSet="utf-8" />
-        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
         <meta
           name="viewport"
           content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no"
         />
-        <link rel="mask-icon" href="/soltanidev.png" color="#35515E" />
-        <link rel="manifest" href="/manifest.json" />
-        <link rel="apple-touch-icon" href="/soltanidev.png" />
-        <link rel="shortcut icon" href="/soltanidev.png" />
-        <meta name="theme-color" content="#35515E" />
-        <meta name="mobile-web-app-capable" content="yes" />
-        <meta name="apple-mobile-web-app-status-bar-style" content="default" />
-        <meta name="msapplication-tap-highlight" content="no" />
-
-        <link rel="apple-touch-startup-image" href="/soltanidev.png" />
       </Head>
       <QueryClientProvider client={reactQueryClient}>
         <ReactQueryDevtools initialIsOpen={false} />
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,4 @@
-import { ArrowBackIcon } from '@chakra-ui/icons';
-import { Box, Button, HStack, Text } from '@chakra-ui/react';
-import { motion } from 'framer-motion';
+import { Box } from '@chakra-ui/react';
 import type { NextPage } from 'next';
 import {
   dehydrate,
@@ -26,21 +24,21 @@ const Home: NextPage = () => {
   >('public', fetchPublic);
   if (isLoading) return <Loading />;
   if (error) return <Box>Error!</Box>;
-  const item = data?.sort((a, b) => a.id - b.id);
+  const item = data?.sort((a, b) => a.id - b.id) ?? [];
 
   return (
     <Box overflowX="hidden">
-      <LandScreen src={item![0]} />
-      <Intro src={item![1]?.info} />
+      <LandScreen src={item?.[0]} />
+      <Intro src={item?.[1]?.info} />
 
-      <Propject src={item!.slice(2, 5)} />
+      <Propject src={item?.slice(2, 5)} />
       <Article
         txt="مقالات یرنامه نویسی"
         page="food"
-        src={item!.slice(11, 15)}
+        src={item?.slice(11, 15)}
       />
-      <Article txt="مقالات عمومی" page="exercise" src={item!.slice(15, 19)} />
-      <Comments src={item![6]?.info} />
+      <Article txt="مقالات عمومی" page="exercise" src={item?.slice(15, 19)} />
+      <Comments src={item?.[6]?.info} />
       <Footer />
     </Box>
   );
