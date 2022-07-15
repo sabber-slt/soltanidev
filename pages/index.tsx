9235511da60ddea4b786db19de90f4a581881810
Sabber-Soltani <108605627+talkboxapplication@users.noreply.github.com>
2022-07-15 23:21:53 +0430

    fix seo

--- pages/index.tsx
+++ pages/index.tsx
@@ -15,7 +15,6 @@ import { fetchPublic } from '../utils/useFetch';
 import Article from '../components/base/Article';
 import Loading from '../components/animation/Loading';
 import DrawerMenu from '../components/base/DrawerMenu';
-import Seo from '../components/Seo';
 
 const Home: NextPage = () => {
   const { data, isLoading, error }: UseBaseQueryResult<IbaseAttributes[]> =
@@ -27,14 +26,6 @@ const Home: NextPage = () => {
 
   return (
     <Box overflowX="hidden">
-      <Seo
-        title="برنامه نویس و طراح انواع اپلیکیشن"
-        desc="طراحی و ساخت انواع وبسایت ها و اپلیکیشن های موبایل "
-        img1="https://res.cloudinary.com/dupfwlkgb/image/upload/v1656669395/soltanidev_s0vc6l.png"
-        img2="https://res.cloudinary.com/dupfwlkgb/image/upload/v1656669245/screen_z0ktaf.png"
-        url=""
-      />
-
       <>
         <DrawerMenu />
         <LandScreen src={item?.[0]?.attributes} />
